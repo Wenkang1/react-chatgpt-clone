9d6ac8eedd04e8e378b567288c01765b37010114
wenkang1 <wenkang1@ualberta.ca>
2023-05-08 22:33:54 +0800

    fanqiang gpt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react"
 
-function App() {
+const App = () => {
+
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [previousChat, setPreviousChat] = useState([])
+  const [currentTitle, setCurrentTitle] = useState([])
+
+  const getMessages = async () =>{
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers:{
+        "Content-Type": "application/json"
+    },
+      
+    }
+    try {
+      const res = await fetch('http://localhost:8000/completions', options)
+      const data = await res.json()
+      console.log(data)
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message){
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message){
+      setPreviousChat(prevChats => (
+        [...prevChats,
+          {
+            title: currentTitle,
+            role:"user",
+            cotent: value
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content
+        }
+      ]
+      ))
+    }
+
+  }, [message, currentTitle, value] )
+  console.log("previousChat: " ,previousChat)
+  console.log("input value: " ,value)
+  console.log("message we got back: " ,message)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>yes</li>
+        </ul>
+        <nav>
+          <p>made by wenkang</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>FanQiangGPT</h1>}
+        <ul className="feed">
+
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>SUBMIT</div>
+          </div>
+          <p className="info">
+            here to store chatgpt info
+          </p>
+        </div>
+
+      </section>
     </div>
   );
 }
